607e6b51427423fa516fceae186f1f586f163612
unimatrix2 <dnny.nis@gmail.com>
2020-10-03 18:06:14 -0300

    adequado ao branch rodrigo

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -3,12 +3,12 @@ const router  = express.Router();
 
 const Produto = require('../models/Produto.model');
 
-router.get('/cardapio', async (req, res, nxt) => {
+router.get('/menu', async (req, res, nxt) => {
 	const cardapio = await Produto.find();
 	res.render('public/menu', { cardapio });
 })
 
-router.get('/cardapio/add', async (req, res, nxt) => {
+router.get('/menu/add', async (req, res, nxt) => {
     const produto = await Produto.findById(req.query.id);
     res.render('/cardapio');
 })

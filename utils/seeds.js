231e6c21fac2400791b8a759dd90821d12a66d93
Danny <62706508+unimatrix2@users.noreply.github.com>
2020-10-04 19:02:37 -0300

    Updated seeds.js

--- utils/seeds.js
+++ utils/seeds.js
@@ -14,7 +14,7 @@ const seed = [
 	{
 		nome: 'Praia da Concha',
 		categoria: 'Lanche',
-		srcImagem: '#',
+		srcImagem: 'https://wessel.com.br/storage/app/uploads/public/5b6/c71/ea0/5b6c71ea08ef7889428492.jpg',
 		descricao: '180 gramas de filé mignon fatiado, molho de gorgonzola e queijo mussarela. Opcional de cebola caramelizada ou puxada na manteiga',
 		ingredientes: ['File Mignon', 'Molho de Gorgonzola', 'Queijo Mussarela', 'Cebola'],
 		preco: 30,
@@ -26,7 +26,7 @@ const seed = [
 	{
 		nome: 'Milionários',
 		categoria: 'Lanche',
-		srcImagem: '#',
+		srcImagem: 'https://wessel.com.br/storage/app/uploads/public/5b2/d07/5e0/5b2d075e05029057326968.jpg',
 		descricao: '200 gramas de hambúrguer de picanha recheada com queijo, bacon, molho da casa, queijo mussarela. Opcional de cebola caramelizada ou puxada na manteiga',
 		ingredientes: ['Hambúrguer de Picanha', 'Bacon', 'Molho da Casa', 'Queijo Mussarela', 'Cebola'],
 		preco: 30,
@@ -38,7 +38,7 @@ const seed = [
 	{
 		nome: 'Olivença (Vegetariano)',
 		categoria: 'Lanche',
-		srcImagem: '#',
+		srcImagem: 'https://i0.statig.com.br/bancodeimagens/8s/ec/6u/8sec6u4vepwqzwmx3mk7b5ryi.jpg',
 		descricao: '200 gramas de salada de berinjela, tomate, alface e queijo mussarela',
 		ingredientes: ['Berinjela', 'Alface', 'Tomate', 'Queijo Mussarela'],
 		preco: 30,
@@ -50,7 +50,7 @@ const seed = [
 	{
 		nome: 'Praia do Cururupe',
 		categoria: 'Lanche',
-		srcImagem: '#',
+		srcImagem: 'https://ohamburguerperfeito.com.br/wp-content/uploads/2014/10/15.png',
 		descricao: '180 gramas de hambúrguer de contra filé, bacon, tomate e cebola roxa, molho da casa, queijo (mussarela ou cheddar)',
 		ingredientes: ['Hambúrguer de Contra Filé', 'Bacon', 'Tomate', 'Cebola Roxa', 'Molho da Casa', 'Queijo Mussarela ou Cheddar'],
 		preco: 28,
@@ -62,7 +62,7 @@ const seed = [
 	{
 		nome: 'Praia do Norte',
 		categoria: 'Lanche',
-		srcImagem: '#',
+		srcImagem: 'https://abrilmdemulher.files.wordpress.com/2018/04/hamburguer-de-cordeiro-com-queijo-de-coalho.jpg?quality=90&strip=info&w=654&h=436&crop=1',
 		descricao: '150 gramas de hambúrguer de carne do sol, bacon, molho da casa e queijo coalho',
 		ingredientes: ['Hambúrguer de Carne do Sol', 'Bacon', 'Molho da Casa', 'Queijo coalho'],
 		preco: 25,
@@ -74,7 +74,7 @@ const seed = [
 	{
 		nome: 'Batuba',
 		categoria: 'Lanche',
-		srcImagem: '#',
+		srcImagem: 'https://s2.glbimg.com/Dm-O7zI7-u1rCihJrvmDrn7_81s=/645x388/i.glbimg.com/og/ig/infoglobo1/f/original/2020/02/10/hamburguer_de_costela_2.jpeg',
 		descricao: '150 gramas de hambúrguer de costela, cebola puxada na manteiga, molho da casa e queijo (mussarela ou cheddar)',
 		ingredientes: ['Hambúrguer de Costela', 'Cebola', 'Molho da Casa', 'Queijo Mussarela ou Cheddar'],
 		preco: 20,
@@ -86,7 +86,7 @@ const seed = [
 	{
 		nome: 'Cristo',
 		categoria: 'Lanche',
-		srcImagem: '#',
+		srcImagem: 'https://wessel.com.br/storage/app/uploads/public/5b2/d09/7d9/5b2d097d9d7ec339330847.jpg',
 		descricao: '150 gramas de hambúrguer de fraldinha, cebola caramelizada, molho da casa e queijo (mussarela ou cheddar)',
 		ingredientes: ['Hambúrguer de Fraldinha', 'Cebola', 'Molho da Casa', 'Queijo Mussarela ou Cheddar'],
 		preco: 20,
@@ -98,7 +98,7 @@ const seed = [
 	{
 		nome: 'Backdoor',
 		categoria: 'Lanche',
-		srcImagem: '#',
+		srcImagem: 'https://i0.statig.com.br/bancodeimagens/2q/oy/jb/2qoyjbjj7figgivfqmqob17jq.jpg',
 		descricao: '150 gramas de frango desfiado, milho, cebola, alface, molho da casa e queijo mussarela',
 		ingredientes: ['Frango Desfiado', 'Milho', 'Cebola', 'Alface', 'Molho da Casa', 'Queijo Mussarela'],
 		preco: 18,
@@ -110,7 +110,7 @@ const seed = [
 	{
 		nome: 'Soares Lopes Junior',
 		categoria: 'Lanche',
-		srcImagem: '#',
+		srcImagem: 'https://f088b146830a59b5.cdn.gocache.net/uploads/noticias/2020/05/27/5d1eb2a4ea88a04f87092e334635c25ee0a084cc.jpg',
 		descricao: '80 gramas de hambúrguer, bacon ou doritos, molho da casa e queijo (mussarela ou cheddar)',
 		ingredientes: ['Hambúrguer', 'Bacon ou Doritos', 'Molho da Casa', 'Queijo Mussarela ou Cheddar'],
 		preco: 13	,
@@ -122,7 +122,7 @@ const seed = [
 	{
 		nome: 'Refrigerante Lata',
 		categoria: 'Bebida',
-		srcImagem: '#',
+		srcImagem: 'https://d3efjz1jvymzgz.cloudfront.net/Custom/Content/Products/10/11/1011792_refrigerante-coca-cola-lata-350ml-fardo-c-12-unidades_z1_637051111791642510.png',
 		descricao: 'Lata 350ml',
 		ingredientes: ['Coca Cola', 'Guaraná', 'Itubaína', 'Schweppes sabores', 'Tônica'],
 		preco: 5,
@@ -137,7 +137,7 @@ const seed = [
 	{
 		nome: 'Refrigerante Litro',
 		categoria: 'Bebida',
-		srcImagem: '#',
+		srcImagem: 'https://casafiesta.fbitsstatic.net/img/p/refrigerante-coca-cola-pet-1l-67784/234652.jpg?w=420&h=420&v=no-change',
 		descricao: 'Garrafa de 1 litro',
 		ingredientes: ['Coca Cola', 'Guaraná', 'Itubaína', 'Dolly sabores'],
 		preco: 9,
@@ -152,7 +152,7 @@ const seed = [
 	{
 		nome: 'Suco 300ml',
 		categoria: 'Bebida',
-		srcImagem: '#',
+		srcImagem: 'https://lh3.googleusercontent.com/proxy/J0gfokFQRK9gRHF-xig7Z1IPfuOqwTMOYO84xvPX_e2UUEWV71zSp_Tw26bfeyTMabMuF2ZSmEkCmsFspGYjv7TNtizbzXW8-2UkzxApu06VlckUiMkcVt0Y8iAInllav5Z3cA',
 		descricao: 'Suco natural coado',
 		ingredientes: ['Laranja', 'Abacaxi com Hortelã', 'Limão', 'Melancia'],
 		preco: 4,
@@ -167,7 +167,7 @@ const seed = [
 	{
 		nome: 'Suco 500ml',
 		categoria: 'Bebida',
-		srcImagem: '#',
+		srcImagem: 'https://lh3.googleusercontent.com/proxy/J0gfokFQRK9gRHF-xig7Z1IPfuOqwTMOYO84xvPX_e2UUEWV71zSp_Tw26bfeyTMabMuF2ZSmEkCmsFspGYjv7TNtizbzXW8-2UkzxApu06VlckUiMkcVt0Y8iAInllav5Z3cA',
 		descricao: 'Suco natural coado',
 		ingredientes: ['Laranja', 'Abacaxi com Hortelã', 'Limão', 'Melancia'],
 		preco: 6,
@@ -182,7 +182,7 @@ const seed = [
 	{
 		nome: 'Água',
 		categoria: 'Bebida',
-		srcImagem: '#',
+		srcImagem: 'https://hiperideal.vteximg.com.br/arquivos/ids/168296-1000-1000/1277910.jpg?v=636615818250200000',
 		descricao: 'Garrafa com ou sem gás',
 		ingredientes: ['Água Mineral Natural'],
 		preco: 3,
@@ -194,7 +194,7 @@ const seed = [
 	{
 		nome: 'Cerveja Lata Puro Malte',
 		categoria: 'Bebida',
-		srcImagem: '#',
+		srcImagem: 'https://trimais.vteximg.com.br/arquivos/ids/996374-1000-1000/foto_original.jpg?v=637309478660870000',
 		descricao: 'Lata 350ml cervejas puro malte',
 		ingredientes: ['Skol Puro Malte', 'Heineken', 'Stella Artois', 'Bohemia', 'Kaiser Gold', 'Eisenbahn Pils', 'Serra Malte'],
 		preco: 6,
@@ -209,7 +209,7 @@ const seed = [
 	{
 		nome: 'Cerveja Long Neck',
 		categoria: 'Bebida',
-		srcImagem: '#',
+		srcImagem: 'https://trimais.vteximg.com.br/arquivos/ids/967178-1000-1000/foto_original.jpg?v=637135125330370000',
 		descricao: 'Cervejas Long Neck',
 		ingredientes: ['Heineken', 'Stella Artois', 'Eisenbahn', 'Corona'],
 		preco: 8,
@@ -224,7 +224,7 @@ const seed = [
 	{
 		nome: 'Batata Frita com Filé',
 		categoria: 'Entradas',
-		srcImagem: '#',
+		srcImagem: 'https://restaurantepururuca.com.br/wp-content/uploads/2016/08/Foto-1-1.jpg',
 		descricao: 'Porção de batatas fritas e contrá filé',
 		ingredientes: ['Batata', 'Contra Filé'],
 		preco: 30	,
@@ -236,7 +236,20 @@ const seed = [
 	{
 		nome: 'Batata Frita com Refri',
 		categoria: 'Entradas',
-		srcImagem: '#',
+		srcImagem: 'https://www.pngkit.com/png/full/315-3155680_poro-de-batata-frita-png.png',
+		descricao: 'Porção de batatas fritas  e um Refri 1 litro',
+		ingredientes: ['Batata', 'Refrigerante'],
+		preco: 20	,
+		peso: '~ 150g',
+		volume: '1L',
+		infoNutricional: { calorias: '~ 570Kcal' },
+		precoDeCusto: 7,
+		disponivel: true
+	},
+	{
+		nome: 'Batata Frita Simples',
+		categoria: 'Entradas',
+		srcImagem: 'https://www.pngkit.com/png/full/315-3155680_poro-de-batata-frita-png.png',
 		descricao: 'Porção de batatas fritas  e um Refri 1 litro',
 		ingredientes: ['Batata', 'Refrigerante'],
 		preco: 20	,
@@ -267,3 +280,6 @@ Produto.create(seed)
 		mongoose.connection.close();
 	})
 	.catch((error) => console.log(error));
+
+
+ module.exports = seed;

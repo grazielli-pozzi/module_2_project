a702118b4ea2922c5bc282ffc339310c2db77da6
unimatrix2 <dnny.nis@gmail.com>
2020-10-05 00:19:12 -0300

    added adicionais to view

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -7,7 +7,8 @@ router.get('/menu', async (req, res, nxt) => {
     const lanches = await Produto.find({ categoria: 'Lanche' });
     const bebidas = await Produto.find({ categoria: 'Bebida' });
     const entradas = await Produto.find({ categoria: 'Entradas' });
-	res.render('public/menu', { lanches, bebidas, entradas });
+    const adicionais = await Produto.find({categoria: 'Adicional'});
+	res.render('public/menu', { lanches, bebidas, entradas, adicionais });
 })
 
 router.get('/menu/add', async (req, res, nxt) => {

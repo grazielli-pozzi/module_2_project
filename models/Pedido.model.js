fd54fad576ca7d0d4b20471fa765d168763f89b4
unimatrix2 <dnny.nis@gmail.com>
2020-10-06 20:18:28 -0300

    touches to pedido model

--- models/Pedido.model.js
+++ models/Pedido.model.js
@@ -6,18 +6,22 @@ const pedidoSchema = new Schema({
 	usuarioID: { type: Schema.Types.ObjectId, ref: 'usuario', required: true },
 	itens: [{
 		produtoID: { type: Schema.Types.ObjectId, ref: 'produto', required: true },
-		quantidade: { type: Number, required: true }
+		quantidade: { type: Number, required: true },
 	   }],
 	observacao: String,
 	pagamento: { type: String, enum: ['Cartao de Credito', 'Cartao de Debito', 'Dinheiro'], required: true },
+	total: { type: Number, required: true },
 	endereco: {
 		cep: { type: String, required: true },
+		estado: { type: String, required: true },
+		cidade: { type: String, required: true },
 		rua: { type: String, required: true },
 		numero: { type: String, required: true }, // String pois há casas com mesmo número mas com A, B, etc no final
 		complemento: String,
 		bairro: { type: String, required: true }
 	},
-	previsaoEntrega: { type: String, required: true, default: '1 hora' }
+	previsaoEntrega: { type: String, required: true, default: '1 hora' },
+	status: { type: String, enum: ['Confirmado', 'Preparando', 'Saiu para Entrega', 'Terminado'], required: true }
 
 }, { timestamps: true });
 

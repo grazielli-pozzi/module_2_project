cbb40088e94977a0dc6b201463427d398631ebe8
unimatrix2 <dnny.nis@gmail.com>
2020-10-07 20:28:08 -0300

    added from branch rodrigo + pedidos user cookie

--- routes/private.routes.js
+++ routes/private.routes.js
@@ -4,13 +4,22 @@ const router  = express.Router();
 const Usuario = require('../models/Usuario.model');
 const Pedido = require('../models/Pedido.model');
 
-router.get('/pedidos', async (req, res) => {
-    console.log('ACESSOU ROTA PEDIDOS');
-    // omitida verificação de cookie if (req.session.currentUser)
-    const usuario = await Usuario.find();
-    const pedidos = await Pedido.find({usuarioID: usuario[0]._id});
+router.get('/cart', (req, res) => {
+    const {sessionExpired} = req.query;
+  
+    res.render('private/cart', {codigoPedido: '000030'});
+});
 
-    res.render('private/pedidos', {pedidos});
+router.get('/pedidos', async (req, res) => {
+    const { sessionExpired } = req.query;
+    console.log(sessionExpired);
+    if (!sessionExpired) {
+        const usuario = await Usuario.find();
+        const pedidos = await Pedido.find({usuarioID: usuario[0]._id});
+        
+        res.render('private/pedidos', {pedidos});
+
+    } else {res.redirect('/'); res.end();}
 });
 
 module.exports = router;

6897f7b485ee957ddf182663547aa1ab1f7a565e
unimatrix2 <dnny.nis@gmail.com>
2020-10-07 02:28:04 -0300

    pedidos route without session/user validation

--- routes/private.routes.js
+++ routes/private.routes.js
@@ -3,14 +3,14 @@ const router  = express.Router();
 
 const Usuario = require('../models/Usuario.model');
 const Pedido = require('../models/Pedido.model');
-const Produto = require('../models/Produto.model');
 
 router.get('/pedidos', async (req, res) => {
     console.log('ACESSOU ROTA PEDIDOS');
     // omitida verificação de cookie if (req.session.currentUser)
-    const usuario = await Usuario.findById(req.query.id);
-    /* const pedidos = [...usuario.pedidos]; */
-    res.render('private/pedidos');
+    const usuario = await Usuario.find();
+    const pedidos = await Pedido.find({usuarioID: usuario[0]._id});
+
+    res.render('private/pedidos', {pedidos});
 });
 
 module.exports = router;

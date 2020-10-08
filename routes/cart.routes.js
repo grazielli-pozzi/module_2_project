638d5e4a6ca93850f6d451e48ac04920bb933d90
unimatrix2 <dnny.nis@gmail.com>
2020-10-07 22:18:44 -0300

    fixed route

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -4,11 +4,19 @@ const router  = express.Router();
 const Produto = require('../models/Produto.model');
 
 router.get('/menu', async (req, res, nxt) => {
-    const lanches = await Produto.find({ categoria: 'Lanche' });
-    const bebidas = await Produto.find({ categoria: 'Bebida' });
-    const entradas = await Produto.find({ categoria: 'Entradas' });
-    const adicionais = await Produto.find({categoria: 'Adicional'});
-	res.render('public/menu', { lanches, bebidas, entradas, adicionais, nome: req.session.currentUser.nomeCompleto });
+    if (req.session.currentUser) {
+        const lanches = await Produto.find({ categoria: 'Lanche' });
+        const bebidas = await Produto.find({ categoria: 'Bebida' });
+        const entradas = await Produto.find({ categoria: 'Entradas' });
+        const adicionais = await Produto.find({categoria: 'Adicional'});
+        res.render('public/menu', { lanches, bebidas, entradas, adicionais, nome: req.session.currentUser.nomeCompleto });
+    } else {
+        const lanches = await Produto.find({ categoria: 'Lanche' });
+        const bebidas = await Produto.find({ categoria: 'Bebida' });
+        const entradas = await Produto.find({ categoria: 'Entradas' });
+        const adicionais = await Produto.find({categoria: 'Adicional'});
+        res.render('public/menu', {lanches, bebidas, entradas, adicionais});
+    }
 })
 
 module.exports = router;

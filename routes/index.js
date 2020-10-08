519b597b172d2814b586f4215337d5dfbcaf383f
unimatrix2 <dnny.nis@gmail.com>
2020-10-07 22:01:35 -0300

    login validation for home route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ const router  = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('public/index');
+  if (req.session.currentUser) {
+    console.log(req.session.currentUser);
+    res.render('public/index', {nome: req.session.currentUser.nomeCompleto});
+  } else {
+    res.render('public/index');
+  }
 });
 
 router.get('/dashboard', (req, res, next) => {

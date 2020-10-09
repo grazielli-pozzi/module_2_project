ab8bf2a54b788dfbfb479b242034cfff1f44ac25
unimatrix2 <dnny.nis@gmail.com>
2020-10-09 11:34:53 -0300

    atualizado da branch rodrigo

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -17,6 +17,53 @@ router.get('/menu', async (req, res, nxt) => {
         const adicionais = await Produto.find({categoria: 'Adicional'});
         res.render('public/menu', {lanches, bebidas, entradas, adicionais});
     }
+});
+
+router.use((req, res, next)=> {
+    if(!req.session.currentUser){
+        res.redirect('/login?sessionExpired=true');
+        return ;
+    }
+    next();
+});
+
+router.get('/cart', async (req, res, nxt) => {
+    try {
+        //console.log(req.session.currentUser.email);
+    
+        const data = await Usuario.find({email: req.session.currentUser.email});
+        
+        //console.log(data);
+
+        //response.render('dashboard', { data, loggedUser: request.session.currentUser });
+        res.render('private/cart', {data});
+    } catch (error) {
+        console.log(error);
+    }
+    // const lanches = await Produto.find({ categoria: 'Lanche' });
+    // const bebidas = await Produto.find({ categoria: 'Bebida' });
+    // const entradas = await Produto.find({ categoria: 'Entradas' });
+
+    //res.render('private/cart', { lanches, bebidas, entradas });
+    //res.render('private/cart');
+})
+
+router.post('/cart', async (req, res, nxt) => {
+    try {
+        console.log(req.body);
+    
+        const data = await Usuario.find({email: req.session.currentUser.email});
+        
+        console.log(data);
+
+        
+
+        //response.render('dashboard', { data, loggedUser: request.session.currentUser });
+        //res.render('private/cart', {data});
+    } catch (error) {
+        console.log(error);
+    }
+
 })
 
 module.exports = router;

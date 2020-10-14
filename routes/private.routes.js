30289d07d2e6ef642f8f9aa72a821b8c3d745947
unimatrix2 <dnny.nis@gmail.com>
2020-10-13 22:23:25 -0300

    moved cart route to cart routes

--- routes/private.routes.js
+++ routes/private.routes.js
@@ -4,13 +4,6 @@ const router  = express.Router();
 const Usuario = require('../models/Usuario.model');
 const Pedido = require('../models/Pedido.model');
 
-router.get('/cart', (req, res) => {
-    if (req.session.currentUser) {
-        res.render('private/cart', {codigoPedido: '000030', nome: req.session.currentUser.nomeCompleto});
-    }
-  
-});
-
 router.get('/pedidos', async (req, res) => {
     if (req.session.currentUser) {
         const usuario = await Usuario.findById(req.session.currentUser._id);

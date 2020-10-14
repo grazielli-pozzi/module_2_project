82376db8774aa13a5a2012c1b1a071be77bf5872
unimatrix2 <dnny.nis@gmail.com>
2020-10-14 19:58:49 -0300

    moved login post route to public routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,74 +1,7 @@
 const express = require('express');
 const router  = express.Router();
 const User = require('../models/Usuario.model');
-const  {generateEncryptedPassword, verifyPassword} = require('../utils/passwordManager');
-
-  const verifyLoginData = async (req, res) =>{
-    const {email, senha} = req.body;
-  
-    if ( !email  || !senha ){
-        const errors = {
-            usernameError: !email ? "Campo Usuário obrigatorio" : undefined,
-            passwordError: !senha ? "Campo password obrigatorio" : undefined,
-        };
-  
-        res.render('public/login', errors);
-  
-        return false;
-    }
-  
-    if (senha.length < 6){
-        const errors = {
-            passwordError: "sua senha deve ter no mínimo 6 digitos!",
-        };
-        res.render('public/login', errors);
-        return false;
-    }
-  
-    const userExists = await User.findOne({ email });
-  
-    //console.log(userExists);
-  
-    if (!userExists) {
-        res.render('public/login', {errorMessage: 'Usuário os senha incorretos. Tente novamente!'});
-  
-        return false;
-    }
-    const passwordOk = await verifyPassword(senha, userExists.senha);
-  
-    if (!passwordOk){
-        res.render('public/login', {errorMessage: 'Usuário os senha incorretos. Tente novamente!'});
-  
-        return false;
-    }
-    
-    return userExists;
-  
-  };
-  
-  router.post('/login',  async (req, res) =>{
-    try{
-        const userOK = await verifyLoginData(req, res);
-        
-        if (!userOK){
-            return ;
-        }
-  
-        //console.log(userOK);
-  
-        const userCopy = JSON.parse(JSON.stringify(userOK));
-  
-        delete userCopy.senha;
-  
-        console.log(userCopy);
-  
-        req.session.currentUser = userCopy;
-  
-        res.redirect('/menu');
-    } catch(error){
-        console.log(error)
-    }
-  });
+const  {generateEncryptedPassword} = require('../utils/passwordManager');
   
   router.post('/signup', async (req, res) =>{
     try {
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router  = express.Router();
 const Produto = require('../models/Produto.model');
+const verifyLoginData = require('../utils/loginManager');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
@@ -32,6 +33,30 @@ router.get('/login',  (req, res) => {
 
   const {sessionExpired} = req.query;
   res.render('public/login', {sessionExpired});
+});
+
+router.post('/login',  async (req, res) =>{
+  try{
+      const userOK = await verifyLoginData(req, res);
+      
+      if (!userOK){
+          return ;
+      }
+
+      //console.log(userOK);
+
+      const userCopy = JSON.parse(JSON.stringify(userOK));
+
+      delete userCopy.senha;
+
+      console.log(userCopy);
+
+      req.session.currentUser = userCopy;
+
+      res.redirect('/menu');
+  } catch(error){
+      console.log(error)
+  }
 });
 
 router.get('/signup', (req, res) =>{

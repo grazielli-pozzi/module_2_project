273d18b5df098e136bcdbb27d54f629d96b03970
Danny <62706508+unimatrix2@users.noreply.github.com>
2020-10-15 20:18:34 -0300

    updated from branch danny

--- routes/private.routes.js
+++ routes/private.routes.js
@@ -3,14 +3,60 @@ const router  = express.Router();
 
 const Usuario = require('../models/Usuario.model');
 const Pedido = require('../models/Pedido.model');
+const { generateEncryptedPassword } = require('../utils/passwordManager');
 
 router.get('/pedidos', async (req, res) => {
-    console.log('ACESSOU ROTA PEDIDOS');
-    // omitida verificação de cookie if (req.session.currentUser)
-    const usuario = await Usuario.find();
-    const pedidos = await Pedido.find({usuarioID: usuario[0]._id});
+    if (req.session.currentUser) {
+        const usuario = await Usuario.findById(req.session.currentUser._id);
+        const pedidos = await Pedido.find({usuarioID: usuario});
+        
+        res.render('private/pedidos', {pedidos, nome: req.session.currentUser.nomeCompleto});
 
-    res.render('private/pedidos', {pedidos});
+    } else {res.redirect('/');}
 });
+
+router.get('/perfil', async (req, res, next) => {
+
+    const {sessionExpired} = req.query;
+  
+    //console.log(sessionExpired);
+  
+    if (!sessionExpired){
+  
+      //console.log('entrou');
+      const data = await Usuario.findOne({email: req.session.currentUser.email});
+      //console.log(data);
+  
+      res.render('private/perfil', {data, nome: req.session.currentUser.nomeCompleto});
+    }else {
+      res.render('public/login', {sessionExpired});
+    };
+  
+  });
+  
+  router.post('/perfil', async (req, res, next) => {
+  
+    try {
+      const {nomeCompleto, email, cpf, telefone, senha, cep, estado, cidade, rua, numero, complemento, bairro} = req.body;
+  
+      let strTel = telefone.replace(/\D/g, '');
+  
+      await Usuario.findByIdAndUpdate(req.session.currentUser._id, {$set: {nomeCompleto,
+        email: req.session.currentUser.email,
+        cpf,
+        telefone: {ddd: strTel.substring(0, 2), numero: strTel.substring(2)},        
+        senha: await generateEncryptedPassword(senha),
+        enderecos: [{cep, estado, cidade, rua, numero, complemento, bairro}],
+        nivel: 'comum',
+        pgtoPadrao: 'Dinheiro'} });
+  
+      res.redirect('/menu');
+  
+  
+    } catch (error) {
+      console.log(error);
+    };
+  
+  });
 
 module.exports = router;
